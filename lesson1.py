04e269c64f032e4a3219fb45d66c870537376555
Kooji707 <bboykooji@gmail.com>
2023-05-13 14:09:59 -0700

    my first commit

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+
+def add (x, y):
+    result = x + y
+    print(result)
+
+
+def sub (x, y):
+    result = x - y
+    print(result)
+my_letter = input("Enter the letter that you want to print: ")
+my_range = int(input("Enter the range: "))
+for x in range(my_range):
+    print(my_letter,end='')
+ 
+ 
+
+#a = int(input("Please enter first number: "))
+#b = int(input("Please enter second number: "))
+
+
+
+ 
