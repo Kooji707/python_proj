adadfcf1bd16bcf853913dfc937b6367daa6c594
Kooji707 <bboykooji@gmail.com>
2023-05-13 22:45:49 -0700

    ggg

---
+++ astrisk_pyramid.py
@@ -0,0 +1,10 @@
+for x in range(5):
+    x = x + 1
+    for y in range(x):
+        print('*',end='')
+    print("")
+
+for a in reversed(range(x)):
+    for b in range(a):
+        print('*',end='')
+    print("")
