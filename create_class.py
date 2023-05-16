e0fda1868005c3c1b302b54fc99e052a2c32de49
Tri Do <trido@Tris-MacBook-Pro-2.local>
2023-05-15 21:27:54 -0700

    yay...

---
+++ create_class.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def Greet(self):
+        print(f'Hello {self.name}')
+        
+per_obj = Person("Vincent", 18)
+
+per_obj.Greet
+per_obj.Greet
+per_obj.Greet
+per_obj.Greet
