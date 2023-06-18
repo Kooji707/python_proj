fbdf7ef0d0707d725e5fbea625cbfa786f8d080d
Kooji707 <bboykooji@gmail.com>
2023-06-18 12:38:39 -0700

    created a func, multiply that accepts multi arg

--- math_functions.py
+++ math_functions.py
@@ -1,5 +1,16 @@
+'''
 import math
 
 x = 3.9
 print(math.ceil(x))
 print(math.floor(x))
+'''
+
+#mylist = (1, 3, 9)
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total = total * number
+    return total
+x_y = multiply(1, 3, 9)
+print(x_y)
